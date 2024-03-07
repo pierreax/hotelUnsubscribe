42e6c647cd1167cf255f05c94514b212f66f161d
pierreaxe <pierre@robotize.nom>
2024-03-07 16:50:03 +0100

    Updates

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ import requests
 # Define the URL for the Sheety API
 url = "https://api.sheety.co/f3a65c5d3619ab6b57dcfe118df98456/flightDeals/prices"
 
-# Send a GET request to retrieve data
-response = requests.get(url)
+# Include your bearer token in the headers
+headers = {
+    'Authorization': 'Bearer 5231bb69-e013-4bf1-97d4-19154d764b31'
+}
+
+# Send a GET request to retrieve data, including the headers with your request
+response = requests.get(url, headers=headers)
 
 if response.status_code == 200:
     data = response.json()
